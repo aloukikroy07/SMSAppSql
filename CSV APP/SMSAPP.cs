905768755a9693fdf6ee99ab1f36dca1acc47a0d
aloukikroy07 <aloukik.roy@gigatechltd.com>
2023-03-13 17:44:24 +0600

    Add project files.

---
+++ CSV APP/SMSAPP.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMSAPP
+{
+    public partial class SMSAPP : Form
+    {
+        public SMSAPP()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string documentType = comboBox1.Text;
+            DateTime fromDate = dateTimePicker1.Value;
+            DateTime toDate = dateTimePicker2.Value;
+
+
+            if (documentType == "Delivery")
+            {
+                Delivery delivery = new Delivery(documentType, fromDate, toDate);
+                delivery.Show();
+            }
+
+            else if (documentType == "Invoice")
+            {
+                Invoice invoice = new Invoice(documentType, fromDate, toDate);
+                invoice.Show();
+            }
+
+            else if (documentType == "Outgoing Payment")
+            {
+                Outgoing_Payments outgoing_Payments = new Outgoing_Payments(documentType, fromDate, toDate);
+                outgoing_Payments.Show();
+            }
+
+            else
+            {
+                MessageBox.Show("Please select SAP document type");
+            }
+
+            //DocumentInfo docInfo = new DocumentInfo();
+            //string message = "স্ট্যান্ডার্ড ফিনিস অয়েল কোং এর ফ্যাক্টোরি থেকে আপনার অর্ডার কৃত পণ্য সমূহের ডেলিভারী করা হয়েছে। আপনার পন্য সমূহ বুঝে নেওয়ার জন্য অনুরোধ করা হলো।";
+
+            //SendSMSSingle ssSingle = new SendSMSSingle(docInfo, message);
+            //ssSingle.Show();
+        }
+
+        private void SMSAPP_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CSV APP/SendSMSSingle.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMSAPP
+{
+    public partial class SendSMSSingle : Form
+    {
+        public SendSMSSingle(DocumentInfo docInfo, string message)
+        {
+            InitializeComponent();
+
+            documentInfo = docInfo;
+            textMessage = message;
+        }
+
+        DocumentInfo documentInfo;
+        string textMessage;
+
+        private void SendSMSSingle_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = documentInfo.DocumentNumber;
+            textBox4.Text = documentInfo.CardName;
+            textBox2.Text = documentInfo.ContactPersonName;
+            textBox3.Text = documentInfo.ContactPersonNumber;
+            richTextBox1.Text = textMessage;
+            textBox8.Text = documentInfo.VehicleNumber;
+            textBox5.Text = documentInfo.DriverName;
+            textBox7.Text = documentInfo.DriverMobile;
+            textBox6.Text = documentInfo.TSOName;
+            textBox9.Text = documentInfo.TSOMobile;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+            label11.Visible = true;
+            SendSMSToCustomer(textBox3.Text, richTextBox1.Text, "CUSTOMER");
+            SendSMSToCustomer(textBox9.Text, richTextBox1.Text, "TSO");
+            pictureBox1.Visible = false;
+            label11.Visible = false;
+        }
+
+        public async void SendSMSToCustomer(string mobileNo, string sms, string person)
+        {
+            string customermobileNo = mobileNo;
+            string message = sms;
+            //string url = "https://masking.zaman-it.com/smsapi?api_key=01720038691.qYGBUjqSF3nlitobAH&type=text&phone=88" + customermobileNo + "&senderid=8809617609850&msg=" + message;
+            string url = "https://sms.zaman-it.com/api/sendsms?api_key=01720038691.qYGBUjqSF3nlitobAH&type=text&phone=88"+customermobileNo+"&senderid=8809617609850&message="+message;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true;
+
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = await request.GetResponseAsync();
+            string json = await new StreamReader(response.GetResponseStream()).ReadToEndAsync();
+
+            if (json == "\r\n\r\n\r\n\r\n\r\n{\"status_code\":200}")
+            {
+                MessageBox.Show("SMS send successfully");
+            }
+
+            else {
+                MessageBox.Show("SMS send failed");
+            }
+        }
+    }
+}
